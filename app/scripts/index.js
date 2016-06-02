10e3916173b70768b5940f52f371e3ce70678b44
Surender Lohia <surender@tenmiles.com>
2016-06-02 12:43:59 +0530

    Initial commit

---
+++ app/scripts/index.js
@@ -0,0 +1,14 @@
+function boot() {
+  angular.bootstrap(document, ['app'], {
+    strictDi: true
+  });
+}
+
+document.addEventListener('DOMContentLoaded', boot);
+
+const {ipcRenderer} = require('electron');
+
+ipcRenderer.on('update-message', function(event, method) {
+    console.log(method);
+    alert(method);
+});
---
+++ main.js
@@ -0,0 +1,32 @@
+const electron = require('electron');
+const {app} = electron;
+const {BrowserWindow} = electron;
+
+let win = null;
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
+
+app.on('ready', createWindow);
+
+function createWindow() {
+  win = new BrowserWindow({
+    name: 'ea-todo',
+    width: 400,
+    height: 600,
+    toolbar: false
+  });
+
+  win.loadURL(`file://${__dirname}/app/index.html`);
+
+  // Open the DevTools.
+  win.webContents.openDevTools({detach: true});
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+
+
